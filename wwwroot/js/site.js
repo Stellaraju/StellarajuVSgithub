360d1cd984d28b222c309eec37c1a3ca5f996502
Stellaraju <stellaraju2004@gmail.com>
2025-01-25 19:26:05 -0800

    musicwebpage

---
+++ wwwroot/js/site.js
@@ -0,0 +1,18 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+window.onload = function () {
+    const title = document.getElementById('title');
+    let opacity = 0;
+
+    // Gradually increase opacity to create fade-in effect
+    const fadeIn = setInterval(function () {
+        if (opacity < 1) {
+            opacity += 0.05;
+            title.style.opacity = opacity;
+        } else {
+            clearInterval(fadeIn);
+        }
+    }, 50);  // Update every 50 milliseconds
+};
